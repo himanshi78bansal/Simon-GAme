d606878dcfaeb40f4d49f455ed107a51289c607d
Himanshi Bansal <101702840+Himanshi7Bansal@users.noreply.github.com>
2024-01-13 01:24:51 +0530

    commit

---
+++ index.js
@@ -0,0 +1,96 @@
+// Simon's game (pattern matching)
+
+// initial values
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+// check keyboard press for restart the game (only one keypress)
+$("body").keypress(function (event) {
+    if (!started) {
+        $("h1").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+// randomly computer generated sequence function
+function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
+    $("h1").text("Level " + level);
+
+    let randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour)
+        .fadeOut(100)
+        .fadeIn(100)
+        .fadeOut(100)
+        .fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+// check user chosen color and send to checkAnswer function
+$(".btn").click(function () {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+// check wheather user pattern matches to computer pattern
+// if yes, then nextSequence will call otherwise
+// game will be over, startOver function will call
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("succes");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+        $("body").addClass("game-over");
+
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+// restart function
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+// play sound function
+function playSound(name) {
+    var audio = new Audio("/sounds/" + name + ".mp3");
+    audio.play();
+}
+
+// shadow animation when click button
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
